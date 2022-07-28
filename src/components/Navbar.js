2940782d30030acc5b75e9118ed37dabb09115a2
Emmans_ B <biodunadekiitan@gmail.com>
2022-07-28 09:14:31 +0100

    first commit to emmans-app

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import reactLogo from "../image/reactjs-icon.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={reactLogo} alt="react logo"/>
+            
+        </nav>
+    )
+}
+
